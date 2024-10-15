f07709bbc416e334c5cc9cfadcd1d77477332080
nicochnz <chiche.nicolas@gmail.com>
2024-10-15 11:08:23 +0200

    node quete

---
+++ index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+console.log(chalk.blue("Hello Manon!"));
+console.log(chalk.green("Hello Thomas!"));
+console.log(chalk.red("Hello Sarah!"));
+console.log(chalk.yellow("Hello Florian!"));
